bea00dcf7af1378a811a6b24cdaf3636a57f8c2c
mohanad atef <158085752+mohanadmvis@users.noreply.github.com>
2024-08-24 20:05:35 +0300

    dashboard

--- resources/views/dashboard/layouts/head.blade.php
+++ resources/views/dashboard/layouts/head.blade.php
@@ -1,6 +1,10 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
+    <title> @hasSection('title')
+            @yield('title')
+        @else
+            home
+        @endif</title>
     <meta
             content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
             name="viewport"
